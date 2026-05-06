3bf2e6ea704959c03bb43a6a45b2a70ed76d5cdb
thaslimShaikh <786shaikthaslim@gmail.com>
2026-05-06 14:36:10 +0530

    Configured backend TypeScript server setup

---
+++ notification_app_be/src/app.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Notification Backend Running",
+  });
+});
+
+export default app;
